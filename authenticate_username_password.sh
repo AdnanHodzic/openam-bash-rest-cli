2e3a56bf87d4df87d8008f5ae10259287d9ac19a
smof <simon_moffatt@warpmail.net>
2013-06-17 16:50:25 +0100

    updated to work better interactively

--- authenticate_username_password.sh
+++ authenticate_username_password.sh
@@ -5,52 +5,70 @@
 #pull in settings file
 source settings
 
-URL="$PROTOCOL://$OPENAM_SERVER:$OPENAM_SERVER_PORT/openam/identity/$FORMAT/authenticate"
+URL="$PROTOCOL://$OPENAM_SERVER:$OPENAM_SERVER_PORT/openam/identity/json/authenticate"
 DATA="username=$1&password=$2"
 
 #check that username is passed as an argument
 if [ "$1" = "" ]; then
+	echo ""
 	echo "Username missing!  Eg $0 bjensen Passw0rd"
+	echo ""
 	exit
 fi
 
 #check that password is passed as an argument
 if [ "$2" = "" ]; then
+	echo ""
 	echo "Password missing!  Eg $0 bjensen Passw0rd"
+	echo ""
 	exit
 fi
 
 #check that curl is present
 CURL_LOC="$(which curl)"
 if [ "$CURL_LOC" = "" ]; then
+	echo ""
 	echo "Curl not found.  Please install sudo apt-get install curl etc..."
+	echo ""
 	exit
 fi
 
 
-if [ "$FORMAT" = "json" ]; then
+#check that jq util is present
+JQ_LOC="$(which jq)"
+if [ "$JQ_LOC" = "" ]; then
+	echo ""
+   	echo "JSON parser jq not found.  Download from http://stedolan.github.com/jq/download/"
+	echo ""
+ 	exit
+fi
 
-	#check that jq util is present
-	JQ_LOC="$(which jq)"
-	if [ "$JQ_LOC" = "" ]; then
-	   	echo "JSON parser jq not found.  Download from http://stedolan.github.com/jq/download/"
-   	exit
-	fi
+echo ""
 
-	./posturl.sh $URL $DATA | jq .
+#./posturl.sh $URL $DATA | jq '.tokenId'
+TOKEN=$(./posturl.sh $URL $DATA | jq '.tokenId')
+
+echo ""
+
+if [ "$TOKEN" = null ]; then
+	
+	echo ""	
+	echo "No token returned.  Check username and password"	
+	echo ""
 
 else
-	#check that xmllint util is present
-	XML_LOC="$(which jq)"
-	if [ "$XML_LOC" = "" ]; then
-	   	echo "XMLLINT parser XML not found."
-	   	exit
-	fi	
-
+	
 	echo ""
-	./posturl.sh $URL $DATA > response.xml
-	echo ""
-	xmllint --format response.xml
+	echo "OpenAM token returned as: $TOKEN"
+	#save token in hidden read-only file		
+	rm -f .token
+	echo $TOKEN > .token
+	chmod 400 .token
+	echo ""		
+	echo "Token saved in read only hidden .token file for later use"
 	echo ""
 
 fi
+
+
+
