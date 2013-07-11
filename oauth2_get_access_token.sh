68933ea2b9f885fc25b4957020d4dd710d5c5989
smof <simon_moffatt@warpmail.net>
2013-07-11 11:14:46 +0100

    updated oauth2_get_access_token to capture client cred

--- oauth2_get_access_token.sh
+++ oauth2_get_access_token.sh
@@ -9,33 +9,59 @@ source settings
 source url_encoder.sh
 
 URL="$PROTOCOL://$OPENAM_SERVER:$OPENAM_SERVER_PORT/openam/oauth2/access_token"
-DATA="grant_type=password&username=$1&password=$2&scope=cn$20mail"
+DATA="grant_type=password&username=$1&password=$2&scope=cn%20mail"
 
 #check that username is passed as an argument
 if [ "$1" = "" ]; then
-	echo "Username missing!  Eg $0 bjensen Passw0rd"
+	echo ""
+	echo "Username missing!  Eg $0 username password clientID clientPassword"
+	echo ""
 	exit
 fi
 
 #check that password is passed as an argument
 if [ "$2" = "" ]; then
-	echo "Password missing!  Eg $0 bjensen Passw0rd"
+	echo ""
+	echo "Password missing!  Eg $0 username password clientID clientPassword"
+	echo ""
 	exit
 fi
+
+#check that client ID is passed as an argument
+if [ "$3" = "" ]; then
+	echo ""
+	echo "Client ID missing!  Eg $0 username password clientID clientPassword"
+	echo ""
+	exit
+fi
+
+#check that client password is passed as an argument
+if [ "$4" = "" ]; then
+	echo ""
+	echo "Client password missing!  Eg $0 username password clientID clientPassword"
+	echo ""
+	exit
+fi
+
 
 #check that curl is present
 CURL_LOC="$(which curl)"
 if [ "$CURL_LOC" = "" ]; then
+	echo ""
 	echo "Curl not found.  Please install sudo apt-get install curl etc..."
+	echo ""
 	exit
 fi
 
 #check that jq util is present
 JQ_LOC="$(which jq)"
 if [ "$JQ_LOC" = "" ]; then
+	echo ""
  	echo "JSON parser jq not found.  Download from http://stedolan.github.com/jq/download/"
+	echo ""
   	exit
 fi
 
-curl --request POST --user "$1:$2" --data $DATA $URL  | jq .
+curl --request POST --user "$3:$4" --data $DATA $URL  | jq .
+
 
